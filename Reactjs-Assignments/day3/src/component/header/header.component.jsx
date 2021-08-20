7b6ca0861e37221a6744b86667d55130641e9c2c
saurabhp8507014288 <74844269+saurabhp8507014288@users.noreply.github.com>
2021-08-20 16:39:33 +0530

    Add files via upload

---
+++ Reactjs-Assignments/day3/src/component/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.styles.css";
+
+const Header = () => {
+	return (
+		<div className="header">
+			<p>Search Movies</p>
+		</div>
+	);
+};
+
+export default Header;
